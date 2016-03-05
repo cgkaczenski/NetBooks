220535cff0ad3196c1d4d74130b1e97abaa4bee1
Christopher Kaczenski <kzpulaski@gmail.com>
2016-03-04 18:15:42 -0600

    Initial commit

---
+++ BootstrapIntro/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BootstrapIntro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Basics()
+        {
+            return View();
+        }
+
+        public ActionResult Advanced()
+        {
+            var person = new Person
+            {
+                FirstName = "Chris",
+                LastName = "Kaczenski"
+            };
+
+            return View(person);
+        }
+    }
+}
---
+++ BootstrapIntro/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace BootstrapIntro.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
